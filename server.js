fa4d089dcac277df9431b17b1494ef3c7bdfcb16
cvellle <cuekisa@yahoo.com>
2021-01-22 17:33:25 +0100

    reload routes

--- server.js
+++ server.js
@@ -12,12 +12,19 @@ const MongoStore = require("connect-mongo")(session);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const cors = require("cors");
+const path = require("path");
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const MONGO_URI =
